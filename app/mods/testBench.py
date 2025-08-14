b43a35794394614da1a7be9f3ea19908e0aa0c55
zbotta <matias.bottarini@gmail.com>
2025-08-14 13:20:50 +0200

    extra print for debugging

--- app/mods/testBench.py
+++ app/mods/testBench.py
@@ -216,8 +216,8 @@ class TestBench:
 
     # generate and obtain title and report from the structured output
     # if empty object, catch the error and give a default value
-    log.info(f"Generating text with the following parameters:\n{gen_param}")
-    print(f"Generating text with the following parameters:\n{gen_param}")
+    log.info(f"Ref_row:{row.name}: Generating text with the following parameters:\n{gen_param}")
+    print(f"Ref_row:{row.name}: Generating text with the following parameters:\n{gen_param}")
     try:
       output, gen_param = report_generator.generate_report(prompt=gen_prompt, **gen_param)
       log.debug(f"\nThe output of the model {self.ml.model_id} is: \n{output}")
